0fb61dc314c38180bcf12d1a2932d9373ed2f1be
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-07-26 19:12:12 +0300

    Реализована функция удаления разделов, включая вложенные темы и сообщения в них.

--- ASP_Forum/Controllers/AdminController.cs
+++ ASP_Forum/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP_Forum.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -34,6 +35,38 @@ namespace ASP_Forum.Controllers
             }
 
             return RedirectToAction("Index", "Admin", new { @error = true });
+        }
+
+        public async Task<IActionResult> DeleteSection(Section section)
+        {
+            Section sectionToDelete = db.Sections.FirstOrDefault(s => s.Name == section.Name);
+            List<Topic> topics = new List<Topic>();
+            List<Reply> replies = new List<Reply>();
+
+            foreach (var topic in db.Topics)
+            {
+                if (topic.SectionId == sectionToDelete.Id)
+                {
+                    topics.Add(topic);
+                }
+            }
+            foreach (var reply in db.Replies)
+            {
+                foreach (var topic in topics)
+                {
+                    if (reply.TopicId == topic.Id)
+                    {
+                        replies.Add(reply);
+                    }
+                }
+            }
+
+            db.Replies.RemoveRange(replies);
+            db.Topics.RemoveRange(topics);
+            db.Sections.Remove(sectionToDelete);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
